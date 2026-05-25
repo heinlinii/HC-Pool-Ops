ec4c0f991601edfb576f5fd89203a9ca7734cf2c
Michael Heinlin <heinlinii@gmail.com>
2026-05-25 12:36:03 -0500

    Switch database driver to psycopg3

--- app/database.py
+++ app/database.py
@@ -11,7 +11,11 @@ if not DATABASE_URL:
 
 
 if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql+psycopg://",
+        1
+    )
 
 
 connect_args = {}
